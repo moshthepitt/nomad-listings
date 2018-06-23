58190c6c5ee1e6cbf0a41b0a65da235c16a5f163
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-23 13:10:49 +0300

    Install mb-term-meta via composer

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'f7d520a7dfd4979ca7d4b88dfbc5d7f5' => $vendorDir . '/rilwis/meta-box/meta-box.php',
+    'efecda00eeb903af145c04079ced01ca' => $baseDir . '/wp-content/plugins/mb-term-meta/mb-term-meta.php',
 );
